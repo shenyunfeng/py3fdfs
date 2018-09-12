88ca39af28200ebc225fb109f318694e156bdb23
avajsun <419733889@qq.com>
2018-09-12 11:34:11 +0800

    no message

--- setup.py
+++ setup.py
@@ -29,8 +29,8 @@ sdict = {
         'License :: GPLV3',
         'Operating System :: OS Independent',
         'Programming Language :: Python'],
-    'ext_modules' : [Extension('fdfs_client.sendfile',
-                             sources = ['fdfs_client/sendfilemodule.c'])],
+    # 'ext_modules' : [Extension('fdfs_client.sendfile',
+    #                          sources = ['fdfs_client/sendfilemodule.c'])],
 }
 
 
